99d9294ba1a8078854ef1d2a394ebe9604d34147
yoon <tyoon9781@gmail.com>
2024-01-24 23:04:52 +0900

    [django add] web page copy practice

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,127 @@
-import Image from "next/image";
+type LinkProps = {
+  contents: string;
+  link?: string;
+  highlightContents?: string;
+};
+
+type CardProps = {
+  image: string;
+  title: string;
+  contents: string;
+};
+
+const Menu = ({
+  link = "#",
+  contents: context,
+  highlightContents: highlightContext = "",
+}: LinkProps) => {
+  return (
+    <li className="text-xs font-bold uppercase">
+      <a href={link}>
+        <span>{context}</span>
+        <span> {highlightContext}</span>
+      </a>
+    </li>
+  );
+};
+
+const Button = ({
+  link = "#",
+  contents: context,
+  highlightContents: highlightContext = "",
+}: LinkProps) => {
+  return (
+    <button className="px-12 py-4 bg-[#44b78b] font-bold rounded-md">
+      <a href={link}>
+        <span className="text-white">{context}</span>
+        <span className="text-[#c9f0DD]"> {highlightContext}</span>
+      </a>
+    </button>
+  );
+};
+
+const Header = () => {
+  return (
+    <div className="bg-[#0C4B33] text-white p-4">
+      <div className="flex justify-between items-center">
+        <div className="flex items-center">
+          <div className="text-4xl font-bold mr-2 pb-2">
+            <a href="#">django</a>
+          </div>
+          <div className="text-xs text-[#44b78b] font-bold w-52">
+            The web framework for perfectionists with deadlines.
+          </div>
+        </div>
+        <ul className="flex justify-between space-x-4">
+          <Menu contents="Overview" />
+          <Menu contents="Download" />
+          <Menu contents="Documentation" />
+          <Menu contents="News" />
+          <Menu contents="Community" />
+          <Menu contents="Code" />
+          <Menu contents="Issues" />
+          <Menu contents="About" />
+          <Menu contents="♥Donate" />
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const Hero = () => {
+  return (
+    <div className="_hero">
+      <div className=" mx-auto text-center pt-12 pb-24 border-b">
+        <p className="py-12 w-[700px] text-[#0c3c26] text-[32px] font-light mx-auto text-center">
+          Django makes it easier to build better web apps more quickly and with
+          less code.
+        </p>
+        <Button contents="Get started with Django" />
+      </div>
+    </div>
+  );
+};
+
+const Card = ({ image, title, contents }: CardProps) => {
+  return (
+    <div>
+      <img src={image} alt={title} />
+      <div>{title}</div>
+      <div>{contents}</div>
+    </div>
+  );
+};
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <title>Django Demo page</title>
+      <Header />
+      <Hero />
+      <div className="_contents flex flex-wrap">
+        <div className="_right w-8/12 px-10 py-6">
+          <h1 className="_contents_title text-3xl my-6">Meet Django</h1>
+          <div className="text-xl font-serif font-extralight mb-6">
+            Django is a high-level Python web framework that encourages rapid
+            development and clean, pragmatic design. Built by experienced
+            developers, it takes care of much of the hassle of web development,
+            so you can focus on writing your app without needing to reinvent the
+            wheel. It’s free and open source.
+          </div>
+          <Card
+            image=""
+            title="Ridiculously fast."
+            contents="Django was designed to help developers take applications from concept to completion as quickly as possible."
+          />
+        </div>
+        <div className="_left w-4/12 p-10">
+          <Button
+            contents="DownLoad"
+            highlightContents="latest release: 5.0.1"
+          />
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <div className="_footer">footer</div>
+    </>
   );
 }
